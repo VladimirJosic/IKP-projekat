79e6f28856034d728eddc61d1a0a6e49cef6ae7d
unknown <nemanjalazarevic0@gmail.com>
2022-12-18 02:59:31 +0100

    Klijent-Server TCP

--- Publisher.c
+++ Publisher.c
@@ -1,9 +1,93 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
 #include <winsock2.h>
-#include <sys/types.h>
-#include <unistd.h> // read(), write(), close()
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT 27016
+
+int InitializeWindowsSockets();
+
+int __cdecl main(void) //int argc, char **argv
+{
+    // socket used to communicate with server
+    SOCKET connectSocket = INVALID_SOCKET;
+    // variable used to store function return value
+    int iResult;
+    char *messageToSend = "this is a test";
+
+    // Validate the parameters
+    // if (argc != 2)
+    // {
+    //     printf("usage: %s server-name\n", argv[0]);
+    //     getch();
+    //     return 1;
+    // }
+
+    if(InitializeWindowsSockets() == -1)
+    {
+		return 1;
+    }
+
+    connectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (connectSocket == INVALID_SOCKET)
+    {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        getch();
+        return 1;
+    }
+
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(DEFAULT_PORT);
+    if (connect(connectSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        printf("Unable to connect to server.\n");
+        closesocket(connectSocket);
+        WSACleanup();
+    }
+    
+    // Send message with null terminator included
+    iResult = send(connectSocket, messageToSend, (int)strlen(messageToSend) + 1, 0);
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("send failed with error: %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        getch();
+        return 1;
+    }
+
+    printf("Bytes Sent: %ld\n", iResult);
+
+    closesocket(connectSocket);
+    WSACleanup();
+
+    getch();
+    
+    return 0;
+}
+
+
+
+
+int InitializeWindowsSockets()
+{
+    WSADATA wsaData;
+	// Initialize windows sockets library for this process
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+    {
+        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+        getch();
+        return -1;
+    }
+	return 0;
+}
